62e00631ec5fa3fced7be8154288a02e987146d0
Kevin Katzer <kevinktzr@gmail.com>
2024-09-13 18:08:36 -0300

    Initial Commit

---
+++ PlacesTests/PlacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesTests.swift
+//  PlacesTests
+//
+//  Created by Kevin Katzer on 13/09/24.
+//
+
+import Testing
+@testable import Places
+
+struct PlacesTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
